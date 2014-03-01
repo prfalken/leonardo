fa740ed59b98fb88b51c0884fb4204abd4ea6d50
Carlos <nzlosh@yahoo.com>
2014-03-01 03:18:29 +0100

    Re-organised a lot of the javascript code so it's inside the object "leo", which represents a namespace within the DOM.
    The query string code is written to make manipulation of key/value
    pairs easier to manipulate.
    
    A query string is used to track if the date picker was opened or closed.

--- leonardo/static/js/selectdate.js
+++ leonardo/static/js/selectdate.js
@@ -1,125 +1,165 @@
-$(document).ready(function() {
-  if($('#dt_from').val() != 'from')
-    $('#toggleDateTimePicker').parent().addClass('active');
-  else
-    $('#dateTimePicker').hide();
-});
+var leo = {
+    qso: Object(),                           // query string object
+    date_from: "-30min",
+    date_to: "now",
+    zeroPad: function(num) {
+        return num < 10 ? String(num) : "0" + num;
+    },
+    getURLParameter : function(name) {
+        return leo.qso[name];
+    },
+    transformQueryStringToObject : function(qs) {
+        /*
+         * Split query string into an array, then process the array for
+         * key/value pairs to be stored as an object, which will later be
+         * used to update/modify the pairs and reconstruct a new query string.
+         */
 
-$(function() {
-  $('#toggleDateTimePicker').click(function() {
-    $('#dateTimePicker').toggle(400);
-  });
-});
+        var qso = {};   // query string array
+        var qsa = qs.split(/[&;=?]/);   // query string array
+        var k="";                       // key name
+        var v="";                       // value
 
-$(function() {
-  var startDateTextBox = $('#dt_from');
-  var endDateTextBox = $('#dt_to');
-  startDateTextBox.datetimepicker({
-    dateFormat: 'yy-mm-dd',
-    onClose: function(dateText, inst) {
-      setDestinationDate(startDateTextBox, endDateTextBox, dateText);
-    }
-  });
-  endDateTextBox.datetimepicker({ 
-    dateFormat: 'yy-mm-dd',
-    onClose: function(dateText, inst) {
-      setDestinationDate(endDateTextBox, startDateTextBox, dateText);
-    }
-  });
-});
+        for ( var i = 0; i < qsa.length; i++ ) {
 
-function getURLParameter(name) {
-  return decodeURIComponent(
-    (RegExp(name + '=' + '(.+?)(&|$)').exec(location.search)||[,""])[1]
-  );
-}
+            if (k == "" && String(qsa[i]) == "") {
+                continue;
+            }
 
-function removeURLParameter(url, parameter) {
-    //prefer to use l.search if you have a location/link object
-    var urlparts= url.split('?');   
-    if (urlparts.length>=2) {
+            if ( k == "" && v == "") {
+                k = String(qsa[i]);
+                continue;
+            }
 
-        var prefix= encodeURIComponent(parameter)+'=';
-        var pars= urlparts[1].split(/[&;]/g);
-
-        //reverse iteration as may be destructive
-        for (var i= pars.length; i-- > 0;) {    
-            //idiom for string.startsWith
-            if (pars[i].lastIndexOf(prefix, 0) !== -1) {  
-                pars.splice(i, 1);
+            if ( k != "" && v == "") {
+                v = String(qsa[i]);
+            }
+            qso[k] = v;                 // assign key/value pair to query string object.
+            k = v = "";                 // re-initialise key/value variables.
+        }
+        return qso;
+    },
+    removeURLParameter : function (qso, parameter) {
+        if ( parameter in qso ) {
+            delete qso[parameter];
+        }
+        return qso
+    },
+    buildGraphiteDateString : function(date) {
+        return Math.round(date.getTime() / 1000);
+    },
+    setDestinationDate : function(srcDateBox, destDateBox, dateText)
+    {
+        if (destDateBox.val() == destDateBox.prop("defaultValue"))
+            destDateBox.val(dateText);
+        else {
+            var startDate = $('#dt_from').datetimepicker('getDate');
+            var endDate = $('#dt_to').datetimepicker('getDate');
+            if (startDate > endDate)
+                destDateBox.datetimepicker('setDate', srcDateBox.datetimepicker('getDate'));
+        }
+    },
+    formatSelectedDate : function(date) {
+        return  zeroPad(date.getMonth() + 1) + "/"
+                + zeroPad(date.getDate()) + "/"
+                + date.getFullYear() + " "
+                + zeroPad(date.getHours()) + ":"
+                + zeroPad(date.getMinutes());
+    },
+    getMethods : function(obj) {
+        var result = [];
+        for (var id in obj) {
+            try {
+                if (typeof(obj[id]) == "function") {
+                    result.push(id + ": " + obj[id].toString());
+                }
+            } catch (err) {
+                result.push(id + ": inaccessible");
             }
         }
+        return result;
+    },
+    selectDt : function() {
+        dt_from = new Date($('#dt_from').val());
+        dt_to = new Date($('#dt_to').val());
+        window.location = leo.buildGraphiteDateUrl(dt_from, dt_to);
+        return true;
+    },
+    buildGraphiteDateUrl : function(dt_from, dt_to) {
+        leo.qso["dt_from"] = leo.buildGraphiteDateString(dt_from);
+        if ( "from" in leo.qso ) delete leo.qso.from;
+        leo.qso["dt_to"]   = leo.buildGraphiteDateString(dt_to);
+        if ( "to" in leo.qso ) delete leo.qso.to;
 
-        url= urlparts[0]+'?'+pars.join('&');
-        return url;
-    } else {
-        return url;
+        newurl = window.location.protocol + "//" + window.location.host
+            + window.location.pathname ;
+
+        querystring = "";
+        if (Object.keys(leo.qso).length > 0) querystring = "?";
+
+        for ( var k in leo.qso ) {
+            querystring += k + "=" + leo.qso[k] + "&";
+        }
+        console.log(newurl + querystring);
+        return newurl + querystring;
+    },
+    toggleSelectDate : function() {
+        $('#dateTimePicker').toggle(400);
+
     }
-}
+};
 
-function setDestinationDate(srcDateBox, destDateBox, dateText)
-{
-  if (destDateBox.val() == destDateBox.prop("defaultValue"))
-    destDateBox.val(dateText);
-  else {
-    var startDate = $('#dt_from').datetimepicker('getDate');
-    var endDate = $('#dt_to').datetimepicker('getDate');
-    if (startDate > endDate)
-      destDateBox.datetimepicker('setDate', srcDateBox.datetimepicker('getDate'));
-  }
-}
+$(document).ready(function() {
+    console.log("jquery V" + $.fn.jquery + "_13");
 
-function formatSelectedDate(date) {
-  return "" + zeroPad(date.getMonth() + 1) + "/" + 
-    zeroPad(date.getDate()) + "/" 
-    + date.getFullYear() + " " 
-    + zeroPad(date.getHours()) + ":" + 
-    zeroPad(date.getMinutes());
-}
+    leo.qso = leo.transformQueryStringToObject(window.location.search);
 
-function getMethods(obj) {
-  var result = [];
-  for (var id in obj) {
-    try {
-      if (typeof(obj[id]) == "function") {
-        result.push(id + ": " + obj[id].toString());
-      }
-    } catch (err) {
-      result.push(id + ": inaccessible");
+    $('#toggleDateTimePicker').parent().addClass('active');
+    $('#dateTimePicker').hide();
+
+    if ( "show_ds" in leo.qso ) {
+        if ( leo.qso.show_ds != String(false) ) {
+            leo.toggleSelectDate();
+            leo.qso.show_ds = true;
+        } else {
+            leo.qso.show_ds = false;
+        }
     }
-  }
-  return result;
-}
+});
 
-function selectDt() {
-  dt_from = new Date($('#dt_from').val());
-  dt_to = new Date($('#dt_to').val());
-  window.location = buildGraphiteDateUrl(dt_from, dt_to);
-  return true;
-}
+$(function() {
+    $('#toggleDateTimePicker').click(function() {
+        leo.toggleSelectDate();
+        leo.qso.show_ds = !leo.qso.show_ds;
+        console.log(leo.qso.show_ds);
+    });
+});
 
-function buildGraphiteDateUrl(dt_from, dt_to)
-{
-  from = buildGraphiteDateString(dt_from);
-  to = buildGraphiteDateString(dt_to);
-  newurl = document.URL.replace(/#/g, '');
-  newurl = removeURLParameter(newurl, "from")
-  newurl = removeURLParameter(newurl, "to")
-  params = "?from=" + from + "&to=" + to;
-  return newurl + params;
-}
+$(function() {
+    var startDateTextBox = $('#dt_from');
+    var endDateTextBox = $('#dt_to');
+    startDateTextBox.datetimepicker({
+        dateFormat: 'yy-mm-dd',
+        onClose: function(dateText, inst) {
+            leo.setDestinationDate(startDateTextBox, endDateTextBox, dateText);
+        }
+    });
+    endDateTextBox.datetimepicker({
+        dateFormat: 'yy-mm-dd',
+        onClose: function(dateText, inst) {
+            leo.setDestinationDate(endDateTextBox, startDateTextBox, dateText);
+        }
+    });
+});
 
-function buildGraphiteDateString(date)
-{
-  // val = zeroPad(date.getHours()) + "%3A" + zeroPad(date.getMinutes()) + "_" + date.getFullYear() + zeroPad(date.getMonth()+1) + zeroPad(date.getDate());
-  val = Math.round(date.getTime() / 1000);
-  return val;
-}
 
-function zeroPad(num) {
-	num = "" + num
-	if (num.length == 1) {
-		num = "0" + num
-	}
-	return num
-}
+
+
+
+
+
+
+
+
+
+
