902c1695fbe881bb128e12e6341b5f5a19be46df
Julien Dehee <julien@dailymotion.com>
2014-03-05 22:51:35 +0100

    Nice 500 error page fixes #13

--- leonardo/views.py
+++ leonardo/views.py
@@ -272,3 +272,7 @@ def multiple(asked_dashboards = None):
 def not_found(error):
   return render_template('404.html'), 404
 
+
+@app.errorhandler(500)
+def server_error(error):
+    return render_template('500.html'), 500
