43e828548799b066e160dfb2deb20cbdad3beae1
Julien Dehee <julien@dailymotion.com>
2014-07-02 15:38:59 +0200

    modify setup.py to discover sub packages

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='Leonardo',
     version='1.0',
     long_description="A Graphite Dashboard, massively inspired by the excellent GDash made by ripienaar",
-    packages=["leonardo"],
+    packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
     install_requires=["pyyaml", "flask"]
